62cf084c43eec82be7e8df7dec1a120652f9ddfb
Pavel Zadorski <pavel.zadorski@gmail.com>
2024-12-28 17:50:32 +0100

    fix missing semicolon

--- wsl.nix
+++ wsl.nix
@@ -33,7 +33,7 @@
       hostNames = ["github.com"];
       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
     };
-  }
+  };
 
   programs.fish.enable = true; # change your shell here if you don't want fish  
   environment.pathsToLink = ["/share/fish"];

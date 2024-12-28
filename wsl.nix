a628c255e05bb562ed5df2e9393d734efcd9fdd3
Pavel Zadorski <pavel.zadorski@gmail.com>
2024-12-28 17:49:48 +0100

    add github fingerprint for ssh validation

--- wsl.nix
+++ wsl.nix
@@ -26,7 +26,15 @@
       paz-user = {};
     };
   };
-    
+
+  # helpful for `ssh -vT git@github.com` to validate key chain 
+  programs.ssh.knownHosts = {
+    github = {
+      hostNames = ["github.com"];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+    };
+  }
+
   programs.fish.enable = true; # change your shell here if you don't want fish  
   environment.pathsToLink = ["/share/fish"];
   environment.shells = [pkgs.fish];
